29fa07ae21f3833aab1e2c08342094ca240e3293
vibhav1011 <vibhav1011@gmail.com>
2023-02-23 01:15:52 +0530

    better api for json

--- api/save.go
+++ api/save.go
@@ -1,36 +1,39 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"io"
-	"io/ioutil"
 	"net/http"
-	"os"
+	"net/http/httputil"
+	"encoding/json"
+	"log"
 )
 
 
 func save_sheet(response http.ResponseWriter, request *http.Request) {
 	fmt.Printf("got /save request\n")
 	
-	body, body_err := ioutil.ReadAll(request.Body)
+	body, body_err := httputil.DumpRequest(request, true)
 	if body_err != nil {
 		fmt.Printf("could not read body: %s\n", body_err)
 	}
 
-	fmt.Printf("saving %s to sheet ...\n", body)
-	io.WriteString(response, "saving to sheet ...\n")
+	fmt.Printf("saving body to sheet : ", string(body))
+
+	response_map := map[string]interface{}{"status": "ok"}
+	response.Header().Set("Content-Type", "application/json")
+	response.WriteHeader(http.StatusOK)
+
+	json.NewEncoder(response).Encode(response_map)
 }
 
 
 func main() {
 	http.HandleFunc("/save", save_sheet)
 
-	err := http.ListenAndServe(":3333", nil)
-	if errors.Is(err, http.ErrServerClosed) {
-		fmt.Printf("server closed\n")
-	} else if err != nil {
-		fmt.Printf("error starting server: %s\n", err)
-		os.Exit(1)
-	}
+	port := 3333
+	var server_status string = fmt.Sprintf("server started on port: %d", port)
+
+	log.Println(server_status)
+	log.Fatal( http.ListenAndServe(fmt.Sprintf(":%d", port), nil) )	
+
 }
